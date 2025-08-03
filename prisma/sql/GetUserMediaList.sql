cbee151fd5834bae710fae1ae3d95f7460953c88
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2025-08-03 18:20:17 +1000

    ~fixed inconsistent ordering

--- prisma/sql/GetUserMediaList.sql
+++ prisma/sql/GetUserMediaList.sql
@@ -5,6 +5,6 @@ SELECT m."id", m."title", m."icon", s."score" as "score"
 FROM "UserMediaScore" s
 INNER JOIN "Media" m ON m."id" = s."mediaID"
 WHERE "userID" = $1::int and s."score" is not null
-ORDER BY s."score" desc
+ORDER BY s."score" desc, m."title" asc
 LIMIT $3
 OFFSET $2::int
