8f8c573c515d2953bf308001d008ff0b37cb2ac6
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2025-08-14 19:35:32 +1000

    ~rollback

--- prisma/sql/UpdateUserMediaAffinity.sql
+++ prisma/sql/UpdateUserMediaAffinity.sql
@@ -1,12 +1,15 @@
 -- @param $1:userID
 -- @param $2:mediaID
-WITH "update" AS (
-	SELECT SUM(s."score"*a."score") / SUM(a."score") as "score"
-	FROM "UserMediaScore" s
-	INNER JOIN "MediaAffinity" a ON a."aID" = LEAST($2::int, s."mediaID") AND a."bID" = GREATEST($2::int, s."mediaID")
-		and a."overlap" > 100 -- exclude low overlap results
-	WHERE s."userID" = $1::int and s."score" is not null
-	HAVING COUNT(*) > 10 -- exclude media with not enough similarities
+WITH "affinity" AS (
+	SELECT "bID" as "userID", "score" FROM "UserAffinity" WHERE "aID" = $1 and "score" is not null
+	UNION ALL
+	SELECT "aID", "score" FROM "UserAffinity" WHERE "bID" = $1 and "score" is not null
+), "update" AS (
+	SELECT SUM(m."score" * a."score") / SUM(a."score") as "score"
+	FROM "UserMediaScore" m
+	INNER JOIN "affinity" a ON a."userID" = m."userID"
+	WHERE "mediaID" = $2::int
+	HAVING COUNT(*) > 10 -- exclude low overlap results
 )
 
 INSERT INTO "UserMediaScore" ("mediaID", "userID", "affinity")
