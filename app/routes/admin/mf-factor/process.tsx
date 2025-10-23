6f118d9a1a943fd458b0ec3dd396c8ba1ff21822
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2025-10-24 09:56:28 +1100

    ~tweaked training

--- app/routes/admin/mf-factor/process.tsx
+++ app/routes/admin/mf-factor/process.tsx
@@ -14,7 +14,7 @@ export async function action({ request, cookie, headers }: RouteContext) {
 	return MakeStream({ render: renderToString, highWaterMark: 1000, abortSignal: request.signal }, Compute);
 }
 
-const LEARNING_RATE = 0.0001;
+const LEARNING_RATE = 0.0002;
 const MAX_STEPS  = 20;
 const SCALE  = 1/1000;
 const PARRALLEL = 100;
@@ -28,7 +28,7 @@ async function Compute(stream: StreamResponse<true>) {
 			<progress style={{ width: "100%" }} max={100}></progress>
 		</div>
 		<div className="user">
-			<progress style={{ width: "100%" }} max={100}></progress>
+			<progress style={{ width: "100%" }} value={0} max={100}></progress>
 		</div>
 		<div className="status"></div>
 	</>);
@@ -37,22 +37,25 @@ async function Compute(stream: StreamResponse<true>) {
 	for (let step=0; step<MAX_STEPS; step++) {
 		const start = Date.now();
 
-		stream.send(".user", "innerHTML", `<progress style="width: 100%" max="${targets.user.length}" />`);
+		stream.send(".user", "innerHTML", `<progress style="width: 100%" value={0} max="${targets.user.length}" />`);
 
 		for (let i=0; i<targets.media.length; i++) {
 			await Promise.all(targets.media[i].map(u => prisma.$queryRawTyped(MfStepMedia(u, LEARNING_RATE))));
+			if (stream.readyState === StreamResponse.CLOSED) return;
 			stream.send(".media", "innerHTML", `<progress style="width: 100%" value="${i+1}" max="${targets.media.length}" />`);
 		}
+		const mediaStats = (await prisma.$queryRawTyped(MfStats('MEDIA')))[0];
+		await prisma.$queryRawTyped(MfStep('MEDIA'));
 
 		for (let i=0; i<targets.user.length; i++) {
 			await Promise.all(targets.user[i].map(u => prisma.$queryRawTyped(MfStepUser(u, LEARNING_RATE))));
+			if (stream.readyState === StreamResponse.CLOSED) return;
 			stream.send(".user", "innerHTML", `<progress style="width: 100%" value="${i+1}" max="${targets.user.length}" />`);
 		}
-
-		const stats = await prisma.$queryRawTyped(MfStats());
-		await prisma.$queryRawTyped(MfStep('MEDIA'));
+		const userStats = (await prisma.$queryRawTyped(MfStats('USER')))[0];
 		await prisma.$queryRawTyped(MfStep('USER'));
 
+		if (stream.readyState === StreamResponse.CLOSED) return;
 		stream.send(".status", "afterbegin", <div style={{
 			marginBlock: '1rem',
 			marginLeft:  '1em',
@@ -65,23 +68,8 @@ async function Compute(stream: StreamResponse<true>) {
 			<b>Step</b>
 			<div>{step+1} of {MAX_STEPS}</div>
 			<div className="text-right">{((Date.now()-start)*SCALE).toFixed(2)} sec</div>
-			{stats.map((s, i) => {
-				const errorValue = s.error || 0;
-				const nextValue  = s.next  || 0;
-
-				let change;
-				if (errorValue === 0) {
-					change = nextValue > 0 ? 100 : 0; // Handle division by zero
-				} else {
-					change = ((nextValue - errorValue) / errorValue) * 100;
-				}
-
-				return <div key={i} className="contents">
-					<b className="text-capital">{s.type.toLowerCase()}</b>
-					<div>Error {nextValue}</div>
-					<div className="text-right">{change > 0 ? '+' : ''}{change.toFixed(2)}%</div>
-				</div>
-			})}
+			{RenderStats(mediaStats)}
+			{RenderStats(userStats)}
 		</div>);
 		stream.send(".iteration", "innerHTML", `<progress style="width: 100%" value="${step+1}" max="${MAX_STEPS}" />`);
 	}
@@ -104,4 +92,23 @@ async function GetTargets() {
 		media: ChunkArray(medias.map(x => x.id), PARRALLEL),
 		user:  ChunkArray(users.map(x => x.id), PARRALLEL),
 	}
+}
+
+
+function RenderStats(s: { type: 'MEDIA' | 'USER', error: number | null, next: number | null }) {
+	const errorValue = s.error || 0;
+	const nextValue  = s.next  || 0;
+
+	let change;
+	if (errorValue === 0) {
+		change = nextValue > 0 ? 100 : 0; // Handle division by zero
+	} else {
+		change = ((nextValue - errorValue) / errorValue) * 100;
+	}
+
+	return <div className="contents">
+		<b className="text-capital">{s.type.toLowerCase()}</b>
+		<div>Error {nextValue}</div>
+		<div className="text-right">{change > 0 ? '+' : ''}{change.toFixed(2)}%</div>
+	</div>
 }
--- prisma/sql/MfStats.sql
+++ prisma/sql/MfStats.sql
@@ -1,3 +1,5 @@
+-- @param $1:type
 SELECT "type", AVG(ABS("error")) as "error", AVG(ABS("nextError")) as "next"
 FROM "MfFactor"
+WHERE "type" = $1
 GROUP BY 1;
